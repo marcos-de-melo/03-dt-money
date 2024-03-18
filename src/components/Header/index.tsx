c2ba79a668eb95f14cb0d9b364ce8cd0cfa931b9
Marcos de Melo <marcdmelo@gmail.com>
2024-03-18 10:25:23 -0300

    Botões de entrada e saída

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,6 +6,7 @@ import {
 
 import logoImg from "../../assets/logo.svg";
 import * as Dialog from "@radix-ui/react-dialog";
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header() {
   return (
@@ -17,14 +18,7 @@ export function Header() {
           <Dialog.Trigger asChild>
             <NewTransactionsButton>Nova transação</NewTransactionsButton>
           </Dialog.Trigger>
-          <Dialog.Portal>
-            <Dialog.Overlay/>
-            <Dialog.Content>
-                <Dialog.Title>Nova Transação</Dialog.Title>
-
-                <Dialog.Close/>
-            </Dialog.Content>
-          </Dialog.Portal>
+          <NewTransactionModal />
         </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
