ad3991815c0b4d84b0ebb90cefc539b9a87c0876
Marcos de Melo <marcdmelo@gmail.com>
2024-03-20 10:32:42 -0300

    Configurar JSON Server

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,4 +1,5 @@
 import * as Dialog from "@radix-ui/react-dialog";
+import * as RadioGroup from "@radix-ui/react-radio-group";
 import styled from "styled-components";
 
 export const Overlay = styled(Dialog.Overlay)`
@@ -67,7 +68,7 @@ export const CloseButton = styled(Dialog.Close)`
   color: ${(props) => props.theme["gray-500"]};
 `;
 
-export const TransactionType = styled.div`
+export const TransactionType = styled(RadioGroup.Root)`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   gap: 1rem;
@@ -78,7 +79,7 @@ interface TransactionTypelButtonProps{
     variant: 'income' | 'outcome';
 }
 
-export const TransactionTypeButton = styled.button<TransactionTypelButtonProps>`
+export const TransactionTypeButton = styled(RadioGroup.Item)<TransactionTypelButtonProps>`
   background: ${(props) => props.theme["gray-700"]};
   padding: 1rem;
   display: flex;
